e71d306b696aa78832f484215d9be0eacadb57f1
Aq1q <micheu334@gmail.com>
2022-07-21 13:35:48 +0200

    Add sliders to choose amount y and x boxes
    Created sliders to choose grid width and height
    Created button to restart grid and sliders

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-
-const sizeButton = document.getElementById('size-prompt')
-
-sizeButton.addEventListener('click', () => {
-    let size = window.prompt("Type size: (default: 16 max: 64)")
-    size = Number(size);
-    if (typeof size != "number" || size == 0 ){
-        size = 16;
-    } else if (size > 64) {
-        size = 64;
-    }
-    createGrid(size);
-}
-)
-
-
-const gridContainer = document.getElementById("grid-container");
-
-function createGrid(size) {
+function createGrid(sizeX, sizeY) {
     
     while (gridContainer.hasChildNodes()) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
     
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
+    for (let i = 0; i < sizeY; i++) {
+        for (let j = 0; j < sizeX; j++) {
             gridContainer.appendChild(document.createElement('div'));
         }  
     }
+
     for(let i = 0; i < gridContainer.children.length; i++) {
         let child = gridContainer.children[i];
         child.classList.add('grid-item');
-        child.style.width = `calc(100%/${size})`;
-        child.style.height = `calc(100%/${size})`;
+        child.style.width = `calc(100%/${sizeX})`;
+        child.style.height = `calc(100%/${sizeY})`;
     }
+
     const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach(item => {
         item.addEventListener('mouseover', () => item.classList.add('item-hovered'))
     })
 }
 
+const xSlider = document.getElementById('sizeX');
+const ySlider = document.getElementById('sizeY');
+
+xSlider.addEventListener('input',  () => {
+    document.getElementById('x-value').innerText = "X: " + xSlider.value;
+    createGrid(xSlider.value, ySlider.value);
+})
+ySlider.addEventListener('input',  () => {
+    document.getElementById('y-value').innerText = "Y: " + ySlider.value;
+    createGrid(xSlider.value, ySlider.value);
+})
+
+const gridContainer = document.getElementById("grid-container");
+createGrid(xSlider.value, ySlider.value);
+
+
+const restartButton = document.getElementById('restart');
+restartButton.addEventListener('click', () => {
+    xSlider.value = 16;
+    document.getElementById('x-value').innerText = "X: " + xSlider.value;
+    
+    ySlider.value = 16;
+    document.getElementById('y-value').innerText = "Y: " + ySlider.value;
+    
+    createGrid(xSlider.value, ySlider.value);
+});
