20db9c98170052e834b132c4522bb8e619f7ec00
Aq1q <micheu334@gmail.com>
2022-07-21 14:07:31 +0200

    Add random color button
    Added random color option to website

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function createGrid(sizeX, sizeY) {
-    
+function createGrid(sizeX, sizeY, color = "black") {
     while (gridContainer.hasChildNodes()) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
     
-    for (let i = 0; i < sizeY; i++) {
+    for(let i = 0; i < sizeY; i++) {
         for (let j = 0; j < sizeX; j++) {
             gridContainer.appendChild(document.createElement('div'));
         }  
@@ -18,25 +17,42 @@ function createGrid(sizeX, sizeY) {
     }
 
     const gridItems = document.querySelectorAll('.grid-item');
-    gridItems.forEach(item => {
-        item.addEventListener('mouseover', () => item.classList.add('item-hovered'))
-    })
+    if(color != "random")
+    {
+        gridItems.forEach(item => {
+            item.addEventListener('mouseover', () => item.style.background = color);
+        })
+    } else {
+        gridItems.forEach(item => {
+            item.addEventListener('mouseover', () => {
+                item.style.background ="#" + Math.floor(Math.random()*16777215).toString(16);
+            });
+        })
+    }
 }
 
 const xSlider = document.getElementById('sizeX');
 const ySlider = document.getElementById('sizeY');
 
+let rainbow;
+
+const randomColor = document.getElementById('random-color');
+randomColor.addEventListener('click', () => {
+    rainbow = "random";
+    createGrid(xSlider.value, ySlider.value, rainbow);
+} );
+
 xSlider.addEventListener('input',  () => {
     document.getElementById('x-value').innerText = "X: " + xSlider.value;
-    createGrid(xSlider.value, ySlider.value);
+    createGrid(xSlider.value, ySlider.value, rainbow);
 })
 ySlider.addEventListener('input',  () => {
     document.getElementById('y-value').innerText = "Y: " + ySlider.value;
-    createGrid(xSlider.value, ySlider.value);
+    createGrid(xSlider.value, ySlider.value, rainbow);
 })
 
 const gridContainer = document.getElementById("grid-container");
-createGrid(xSlider.value, ySlider.value);
+createGrid(xSlider.value, ySlider.value, rainbow);
 
 
 const restartButton = document.getElementById('restart');
@@ -47,5 +63,6 @@ restartButton.addEventListener('click', () => {
     ySlider.value = 16;
     document.getElementById('y-value').innerText = "Y: " + ySlider.value;
     
+    rainbow = undefined;
     createGrid(xSlider.value, ySlider.value);
 });
